2ec6f407a1cba1f9246425891be0f3f0f741ad02
chloe alcorn <ca.alcorn@outlook.com>
2018-08-08 11:50:33 +0100

    methods added to customer

--- customer.rb
+++ customer.rb
@@ -1,11 +1,19 @@
 class Customer
 
-attr_reader(:customer_name,:wallet)
-attr_writer(:wallet)
+  attr_reader(:customer_name,:wallet)
+  attr_writer(:wallet)
 
-def initialize(customer_name,wallet)
-@customer_name = customer_name
-@wallet = wallet
-end
+  def initialize(customer_name,wallet)
+    @customer_name = customer_name
+    @wallet = wallet
+  end
+
+  def buys_drink(pub,drink)
+    price = drink.price
+    @wallet -= price
+    pub.till += price
+  end
+
+
 
 end
--- specs/customer_specs.rb
+++ specs/customer_specs.rb
@@ -1,19 +1,33 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../customer.rb")
+require_relative("../pub.rb")
+require_relative("../drink.rb")
 
 class Customertest < MiniTest::Test
 
-def setup()
-  @person = Customer.new("Dave",50)
-end
+  def setup()
+    @drink1 = Drink.new("Tennants",2)
+    @drink2 = Drink.new("Cider",3)
+    @drink3 = Drink.new("Beer",4)
+    @person = Customer.new("Dave",50)
+    @pub = Pub.new("Chanter",10,[@drink1,@drink2,@drink3])
 
-def test_customer_has_name
-  assert_equal("Dave",@person.customer_name)
-end
+  end
 
-def test_customer_has_money
-  assert_equal(50, @person.wallet)
-end
+  def test_customer_has_name
+    assert_equal("Dave",@person.customer_name)
+  end
+
+  def test_customer_has_money
+    assert_equal(50, @person.wallet)
+  end
+
+  def test_customer_buys_drink
+    @person.buys_drink(@pub,@drink2)
+    assert_equal(47,@person.wallet)
+    assert_equal(13, @pub.till)
+  end
+
 
 end
