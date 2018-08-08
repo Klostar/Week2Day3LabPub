f86df2a956a348a7238e3025d2ac25dd834a87b1
chloe alcorn <ca.alcorn@outlook.com>
2018-08-08 12:00:57 +0100

    age added and tested

--- customer.rb
+++ customer.rb
@@ -1,19 +1,24 @@
 class Customer
 
-  attr_reader(:customer_name,:wallet)
+  attr_reader(:customer_name,:wallet,:age)
   attr_writer(:wallet)
 
-  def initialize(customer_name,wallet)
+  def initialize(customer_name,wallet,age)
     @customer_name = customer_name
     @wallet = wallet
+    @age = age
   end
 
   def buys_drink(pub,drink)
+if @age >= 18
+
     price = drink.price
     @wallet -= price
     pub.till += price
+  else
+    return "Come back when you're 18!"
   end
-
+end
 
 
 end
--- specs/customer_specs.rb
+++ specs/customer_specs.rb
@@ -10,7 +10,8 @@ class Customertest < MiniTest::Test
     @drink1 = Drink.new("Tennants",2)
     @drink2 = Drink.new("Cider",3)
     @drink3 = Drink.new("Beer",4)
-    @person = Customer.new("Dave",50)
+    @person = Customer.new("Dave",50,20)
+    @person2 = Customer.new("Junior",10,10)
     @pub = Pub.new("Chanter",10,[@drink1,@drink2,@drink3])
 
   end
@@ -23,11 +24,20 @@ class Customertest < MiniTest::Test
     assert_equal(50, @person.wallet)
   end
 
+  def test_customer_has_age
+    assert_equal(20, @person.age)
+  end
+
   def test_customer_buys_drink
     @person.buys_drink(@pub,@drink2)
     assert_equal(47,@person.wallet)
     assert_equal(13, @pub.till)
   end
+
+def test_customer_buys_drink__underage
+  result = @person2.buys_drink(@pub,@drink2)
+  assert_equal("Come back when you're 18!",result)
+end
 
 
 end
